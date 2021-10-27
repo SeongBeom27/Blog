6f9b27ab84e228593ef9f664024ca489f5387214
SeongBeom27 <lgh121546@naver.com>
2021-10-27 20:10:39 +0900

    [학습 사항] 1. 라우트 파라미터와 쿼리

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -14,8 +14,29 @@ rounter.get('/', (ctx) => {
   ctx.body = '홈';
 });
 
-rounter.get('/about', (ctx) => {
-  ctx.body = '소개';
+/**
+ * 라우터 파라미터 설정 방법 => :name 형식으로 콜론을 사용하여 라우트 경로를 설정한다.
+ * 또한 파라미터가 있을 수도 있고 없을 수도 있으면 :name?에 ?를 붙인다
+ * 이렇게 설정한 파라미터는 함수의 ctx.params 객체에서 조회가 가능하다
+ *
+ *  URL 쿼리의 경우 다음과 같이 조회가 가능
+ *  문자,정수 : ctx.query
+ *  문자열 : ctx.querystring
+ *
+ *  test url : http://localhost:4000/about/react
+ */
+rounter.get('/about/:name?', (ctx) => {
+  const { name } = ctx.params;
+  // name의 존재 유무에 따라 다른 결과 출력
+  ctx.body = name ? `${name}의 소개` : '소개';
+});
+
+// test url : http://localhost:4000/posts?id=10
+rounter.get('/posts', (ctx) => {
+  const { id } = ctx.query;
+
+  // name의 존재 유무에 따라 다른 결과 출력
+  ctx.body = id ? `${id}의 소개` : '포스트 아이디가 없습니다.';
 });
 
 // app 인스턴스에 라우터 적용
