57614b986964992965d2f2df3f28bb70353a9316
SeongBeom27 <lgh121546@naver.com>
2021-10-30 13:15:25 +0900

    esm 설치 후 적용
    Node js 에서 import/export 문법이 정식적으로 지원되지 않아서 다음 두가지 설정이 필요하다.
    
    1. 확장자를 .mjs
    2. node 실행 시 --experiment-modules 옵션 추가
    
    혹은, esm이라는 라이브러리의 도움을 받아 해당 문법을 사용하면 가능하다

--- blog-backend/src/index.js
+++ blog-backend/src/index.js
@@ -1,46 +1,5 @@
-require('dotenv').config();
-const Koa = require('koa');
-const Rounter = require('koa-router');
-const bodyParser = require('koa-bodyparser');
-const mongoose = require('mongoose');
+// 이 파일에서만 no-global-assign ESLint 옵션을 비활성화
+/* eslint-disable no-global-assign */
 
-const api = require('./api');
-
-const { PORT, MONGO_URI } = process.env;
-
-mongoose
-  .connect(MONGO_URI, { useNewUrlParser: true })
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((e) => {
-    console.error(e);
-  });
-
-const app = new Koa();
-const router = new Rounter();
-
-// 라우터 설정
-router.use('/api', api.routes());
-
-// 라우터 적용 전에 bodyParser 적용
-app.use(bodyParser());
-
-// app 인스턴스에 라우터 적용
-app.use(router.routes()).use(router.allowedMethods());
-
-const port = PORT || 4000;
-app.listen(port, () => {
-  console.log('Listening to port %d', port);
-});
-
-// koa는 미들웨어의 배열로 구성되어 있다.
-// app.use는 미들웨어 함수를 애플리케이션에 등록시켜준다.
-
-// 미들웨어 함수의 구조
-// (ctx, next) -> {
-// }
-
-// 미들웨어 함수는 두 개의 파라미터를 얻는다.
-// ctx   : Context의 줄임말로
-// next  : 현재 처리 중인 미들웨어의 다음 미들웨어를 호출하는 함수
+require = require('esm')(module /*, options */);
+module.exports = require('./main');
