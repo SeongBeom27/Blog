3e9249910d29a4322b7149e758ba677e5029ecb8
SeongBeom27 <lgh121546@naver.com>
2021-11-04 20:28:28 +0900

    mongoDB : 페이지네이션 구현

--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -61,10 +61,33 @@ export const write = async (ctx) => {
  * GET /api/posts
  */
 export const list = async (ctx) => {
+  // query는 문자열이기 때문에 숫자로 변환 작업이 필요함
+  // 값이 주어지지 않았다면 1을 기본적으로 사용한다
+  const page = parseInt(ctx.query.page || '1', 10);
+
+  if (page < 1) {
+    ctx.status = 400;
+    return;
+  }
+
   try {
     // find 호출 뒤 exec까지 호출해야 쿼리 요청이 된다.
-    const posts = await Post.find().exec();
-    ctx.body = posts;
+    // sort : key는 정렬할 필드를 설정하는 부분이며 1이면 오름차순 -1이면 내림차순
+    // limit : 한 번에 보이는 포스트의 최대 개수를 제한한다
+    const posts = await Post.find()
+      .sort({ _id: -1 })
+      .limit(10)
+      .skip((page - 1) * 10)
+      .exec();
+    const postCount = await Post.countDocuments().exec();
+    ctx.set('Last-Page', Math.ceil(postCount / 10));
+    ctx.body = posts
+      .map((post) => post.toJSON())
+      .map((post) => ({
+        ...post,
+        body:
+          post.body.length < 200 ? post.body : `${post.body.slice(0, 200)}...`,
+      }));
   } catch (e) {
     ctx.throw(500, e);
   }
