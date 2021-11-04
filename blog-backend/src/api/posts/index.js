8cf86591627dea3d1b751bba8a53d824118344c5
SeongBeom27 <lgh121546@naver.com>
2021-11-04 19:20:45 +0900

    mongoDB : 데이터 조회, 삭제, 업데이트, 요청 검증 (ObjectId)

--- blog-backend/src/api/posts/index.js
+++ blog-backend/src/api/posts/index.js
@@ -3,18 +3,10 @@ import * as postsCtrl from './posts.ctrl';
 
 const posts = new Rounter();
 
-const printinfo = (ctx) => {
-  ctx.body = {
-    method: ctx.method,
-    path: ctx.path,
-    params: ctx.params,
-  };
-};
-
 posts.get('/', postsCtrl.list);
 posts.post('/', postsCtrl.write);
-posts.get('/:id', postsCtrl.read);
-posts.delete('/:id', postsCtrl.remove);
-posts.patch('/:id', postsCtrl.update);
+posts.get('/:id', postsCtrl.CheckObjectId, postsCtrl.read);
+posts.delete('/:id', postsCtrl.CheckObjectId, postsCtrl.remove);
+posts.patch('/:id', postsCtrl.CheckObjectId, postsCtrl.update);
 
 module.exports = posts;
--- blog-backend/src/api/posts/posts.ctrl.js
+++ blog-backend/src/api/posts/posts.ctrl.js
@@ -1,4 +1,17 @@
 import Post from '../../models/post';
+import mongoose from 'mongoose';
+
+const { ObjectId } = mongoose.Types;
+
+// ObjectId 검증
+export const CheckObjectId = (ctx, next) => {
+  const { id } = ctx.params;
+  if (!ObjectId.isValid(id)) {
+    ctx.status = 400;
+    return;
+  }
+  return next();
+};
 
 /**
  * 포스트 작성
@@ -32,22 +45,76 @@ export const write = async (ctx) => {
  * 포스트 목록 조회
  * GET /api/posts
  */
-export const list = () => {};
+export const list = async (ctx) => {
+  try {
+    // find 호출 뒤 exec까지 호출해야 쿼리 요청이 된다.
+    const posts = await Post.find().exec();
+    ctx.body = posts;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
 
 /**
  * 특정 포스트 조회
  * GET /api/posts/:id
  */
-export const read = () => {};
+export const read = async (ctx) => {
+  const { id } = ctx.params;
+  try {
+    const post = await Post.findById(id).exec();
+    if (!post) {
+      ctx.status = 404; // not found
+      return;
+    }
+    ctx.body = post;
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
 
 /**
  * 특정 포스트 제거
  * DELETE /api/posts/:id
  */
-export const remove = () => {};
+export const remove = async (ctx) => {
+  /**
+   * 데이터 삭제 함수
+   *
+   * remove : 특정 조건을 만족하는 데이터를 모두 지운다.
+   * findByIdAndRemove : id를 찾아서 지운다.
+   * findOneAndRemove : 특정 조건을 만족하는 데이터 하나를 찾아서 제거한다.
+   */
+  const { id } = ctx.params;
+  try {
+    await Post.findByIdAndRemove(id).exec();
+    ctx.status = 204; // No Content (성공하기는 했지만 응답할 데이터는 없음)
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
 
 /**
  * 포스트 수정 (특정 필드 변경)
  * PATCH /api/posts/:id
+ * {
+ *    title: '수정'
+ *    body:  '수정 내용'
+ *    tags:   ['수정','태그']
+ * }
  */
-export const update = () => {};
+export const update = async (ctx) => {
+  const { id } = ctx.params;
+  try {
+    const post = await Post.findByIdAndUpdate(id, ctx.request.body, {
+      new: true, // 이 값을 설정하면 업데이트된 데이터를 반환한다.
+      // false일 때는 업데이트되기 전의 데이터를 반환한다
+    }).exec();
+    if (!post) {
+      ctx.status = 404;
+      return;
+    }
+  } catch (e) {
+    ctx.throw(500, e);
+  }
+};
