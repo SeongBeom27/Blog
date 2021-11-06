7f52784810d912a6a26d79d8667cec2e2995acb5
SeongBeom27 <lgh121546@naver.com>
2021-11-06 15:17:00 +0900

    mongoDB : 회원 인증 api 생성

--- blog-backend/src/api/index.js
+++ blog-backend/src/api/index.js
@@ -1,9 +1,11 @@
 const Rounter = require('koa-router');
 const posts = require('./posts');
+const auth = require('./auth');
 
 const api = new Rounter();
 
 api.use('/posts', posts.routes());
+api.use('/auth', auth.routes());
 
 // 라우터를 내보냄
 module.exports = api;
