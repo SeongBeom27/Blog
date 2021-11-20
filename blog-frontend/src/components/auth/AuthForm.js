4134819fcc7187daa993d6276664569c7027870d
SeongBeom27 <lgh121546@naver.com>
2021-11-20 21:37:39 +0900

    Front-End : 회원 가입, 로그인 error 처리

--- blog-frontend/src/components/auth/AuthForm.js
+++ blog-frontend/src/components/auth/AuthForm.js
@@ -9,9 +9,18 @@ const textMap = {
 };
 
 /**
+ * 에러를 보여 줌
+ */
+const ErrorMessage = styled.div`
+  color: red;
+  text-align: center;
+  font-size: 0.875rem;
+  margin-top: 1rem;
+`;
+
+/**
  * 회원가입 또는 로그인 폼을 보여줍니다.
  */
-
 const AuthFormBlock = styled.div`
   h3 {
     margin: 0;
@@ -58,7 +67,7 @@ const ButtonWithMarginTop = styled(Button)`
   margin-top: 1rem;
 `;
 
-const AuthForm = ({ type, form, onChange, onSubmit }) => {
+const AuthForm = ({ type, form, onChange, onSubmit, error }) => {
   const text = textMap[type];
   return (
     <AuthFormBlock>
@@ -89,6 +98,7 @@ const AuthForm = ({ type, form, onChange, onSubmit }) => {
             value={form.passwordConfirm}
           />
         )}
+        {error && <ErrorMessage>{error}</ErrorMessage>}
         <ButtonWithMarginTop cyan fullWidth>
           {text}
         </ButtonWithMarginTop>
